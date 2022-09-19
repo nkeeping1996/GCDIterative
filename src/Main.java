c3b34a52b035909f2017efcda35c69a4bb06510e
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-19 12:47:11 -0400

    Simplified code - the separate branches were not necessary. Modular arithmatic returns the same value regardless of whether the larger integer is passed first or second!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(Gcd(90,48));
+
     }
-}
+
+    public static int Gcd(int a, int b){
+        int r;
+        while((r=a%b) > 0){
+            int c = b;
+            b = a%b;
+            a = c;
+        }
+        return b;
+        }
+    }
